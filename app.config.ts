69214e668ad40d3436bfbf4f28b61940c2a6d52d
mikey <mike.owino40@yahoo.com>
2022-11-19 21:49:34 +0000

    config

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,45 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus'
+    title: 'Docus',
+    description: 'My Docus Project',
+    url: 'http://docus.dev',
+    image: '/social-card-preview.png',
+    socials: {
+      twitter: '@mikeyhodl',
+      github: 'weknow-io/weknow',
+    },
+    github: {
+      root: 'content',
+      edit: true,
+      contributors: false
+    },
+    aside: {
+      level: 1,
+      filter: [],
+    },
+    header: {
+      title: false,
+      logo: true,
+      showLinkIcon: false
+    },
+    footer: {
+      credits: {
+        icon: 'IconDocus',
+        text: 'By Mikey',
+        href: 'https://mikeowino.com',
+      },
+      // icons: [
+      //   {
+      //     label: 'NuxtJS',
+      //     href: 'https://nuxtjs.org',
+      //     component: 'IconNuxtLabs',
+      //   },
+      //   {
+      //     label: 'Vue Telescope',
+      //     href: 'https://vuetelescope.com',
+      //     component: 'IconVueTelescope',
+      //   },
+      // ],
+    }
   }
 })
